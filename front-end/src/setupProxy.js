150549d9e2d1ef6d28128f946f11c07c317531df
Nicholas Adamou <nicholasadamouemail@gmail.com>
2022-02-13 12:50:25 -0500

    updated proxy

--- front-end/src/setupProxy.js
+++ front-end/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
     app.use(
         createProxyMiddleware('/api', {
-            target: 'http://upload-service:8080/', // needs to be 8080 to work within docker
+            target: 'http://localhost:82/', // needs to be upload-service:8080 to work within docker
             pathRewrite: {
                 '^/api': '',
             },
