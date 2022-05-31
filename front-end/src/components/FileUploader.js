1408469cc8525e5438ae1b654da7534629ffe0ec
Nicholas Adamou <nicholasadamouemail@gmail.com>
2022-05-31 06:26:01 -0400

    Fix formatting in FileUploader.js

--- front-end/src/components/FileUploader.js
+++ front-end/src/components/FileUploader.js
@@ -2,240 +2,273 @@ import React from 'react';
 
 import styled from 'styled-components';
 
-import {FileUploaderDropContainer, FileUploaderItem} from 'carbon-components-react';
+import {
+    FileUploaderDropContainer,
+    FileUploaderItem,
+} from 'carbon-components-react';
 
 import WorkerBuilder from '../workers/worker-builder';
 import PakoWorker from '../workers/pako.worker';
 
-import {NotificationContext} from '../contexts/NotifiationContext/NotificationContext';
+import { NotificationContext } from '../contexts/NotifiationContext/NotificationContext';
 
 import ChunkedFile from './ChunkedFile';
 
 import { toUint8Array } from '../utilities';
 
 const Container = styled.div`
-  strong {
-	display: block;
+    strong {
+        display: block;
 
-	margin-bottom: 10px;
+        margin-bottom: 10px;
 
-	font-size: 16px;
-	font-weight: bold;
-  }
+        font-size: 16px;
+        font-weight: bold;
+    }
 
-  p {
-	font-size: 16px;
+    p {
+        font-size: 16px;
 
-	margin-bottom: 10px;
-  }
+        margin-bottom: 10px;
+    }
 
-  .bx--file-browse-btn {
-	max-width: 35rem !important;
-  }
+    .bx--file-browse-btn {
+        max-width: 35rem !important;
+    }
 
-  .bx--file__drop-container {
-	font-size: 16px;
+    .bx--file__drop-container {
+        font-size: 16px;
 
-	height: 10rem !important;
-  }
+        height: 10rem !important;
+    }
 `;
 
 const FileUploaderItems = styled.div`
-  margin: 10px 0 20px;
+    margin: 10px 0 20px;
 
-  .bx--file__selected-file {
-	max-width: 35rem !important;
+    .bx--file__selected-file {
+        max-width: 35rem !important;
 
-	p {
-	  margin-bottom: 0;
-	}
-  }
+        p {
+            margin-bottom: 0;
+        }
+    }
 `;
 
 const FileUploader = (props) => {
-	const [file, setFile] = React.useState(undefined);
+    const [file, setFile] = React.useState(undefined);
 
-	const validTypes = [
-		'application/vnd.ms-excel',
-		'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-		'application/zip',
-		'application/x-compressed',
-		'application/x-zip-compressed',
-		'multipart/x-zip',
-		'application/msword',
-		'application/vnd.ms-powerpoint',
-		'application/vnd.openxmlformats-officedocument.presentationml.presentation',
-		'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-		'text/plain',
-		'application/pdf',
-		'application/vnd.ms-excel.sheet.macroenabled.12',
-		'application/vnd.ms-excel.sheet.macroEnabled.12',
-		'application/vnd.ms-excel.sheet.binary.macroenabled.12',
-		'application/vnd.ms-excel.sheet.binary.macroEnabled.12',
-		'text/csv'
-	];
+    const validTypes = [
+        'application/vnd.ms-excel',
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        'application/zip',
+        'application/x-compressed',
+        'application/x-zip-compressed',
+        'multipart/x-zip',
+        'application/msword',
+        'application/vnd.ms-powerpoint',
+        'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+        'text/plain',
+        'application/pdf',
+        'application/vnd.ms-excel.sheet.macroenabled.12',
+        'application/vnd.ms-excel.sheet.macroEnabled.12',
+        'application/vnd.ms-excel.sheet.binary.macroenabled.12',
+        'application/vnd.ms-excel.sheet.binary.macroEnabled.12',
+        'text/csv',
+    ];
 
-	const {showNotification} = React.useContext(NotificationContext);
+    const { showNotification } = React.useContext(NotificationContext);
 
-	const isFileSizeValid = (file) => file.size <= 6000000;
+    const isFileSizeValid = (file) => file.size <= 6000000;
 
-	const handleWorker = async (file, message) => {
-		let fileNameParts = file.name.split('.');
+    const handleWorker = async (file, message) => {
+        let fileNameParts = file.name.split('.');
 
-		let name = fileNameParts[0];
-		const originalExtension = fileNameParts[1];
-		const originalFileName = `${name}.${originalExtension}`;
+        let name = fileNameParts[0];
+        const originalExtension = fileNameParts[1];
+        const originalFileName = `${name}.${originalExtension}`;
 
-		name = `${file.name.split('.')[0]}`;
-		const type = 'application/gzip';
-		const extension = `${originalExtension}.gz`;
+        name = `${file.name.split('.')[0]}`;
+        const type = 'application/gzip';
+        const extension = `${originalExtension}.gz`;
 
-		const buffer = message.data;
-		const blob = new Blob([buffer], { type });
+        const buffer = message.data;
+        const blob = new Blob([buffer], { type });
 
-		await prepareForUpload(name, extension, type, originalFileName, originalExtension, file, blob);
-	}
+        await prepareForUpload(
+            name,
+            extension,
+            type,
+            originalFileName,
+            originalExtension,
+            file,
+            blob
+        );
+    };
 
-	const handleOnDragNDrop = async (e, data) => {
-		const { addedFiles } = data;
+    const handleOnDragNDrop = async (e, data) => {
+        const { addedFiles } = data;
 
-		const file = addedFiles[0];
+        const file = addedFiles[0];
 
-		if (!file) return;
+        if (!file) return;
 
-		if (!isFileSizeValid(file)) {
-			showNotification({
-				success: false,
-				kind: 'error',
-				subtitle: `File size exceeds limit. 6MB max file size. Select a new file and try again.`,
-				timeout: 5000,
-			});
+        if (!isFileSizeValid(file)) {
+            showNotification({
+                success: false,
+                kind: 'error',
+                subtitle: `File size exceeds limit. 6MB max file size. Select a new file and try again.`,
+                timeout: 5000,
+            });
 
-			return;
-		}
+            return;
+        }
 
-		const compressionThreshold = 1000000; // 1MB
+        const compressionThreshold = 1000000; // 1MB
 
-		let shouldCompress = file.size > compressionThreshold;
+        let shouldCompress = file.size > compressionThreshold;
 
-		if (shouldCompress) {
-			const worker = new WorkerBuilder(PakoWorker);
-			worker.postMessage(file);
+        if (shouldCompress) {
+            const worker = new WorkerBuilder(PakoWorker);
+            worker.postMessage(file);
 
-			worker.onmessage = (message) => {
-				handleWorker(file, message);
-			}
+            worker.onmessage = (message) => {
+                handleWorker(file, message);
+            };
 
-			return;
-		}
+            return;
+        }
 
-		let fileNameParts = file.name.split('.');
+        let fileNameParts = file.name.split('.');
 
-		let name = fileNameParts[0];
-		const originalExtension = fileNameParts[1];
-		const originalFileName = `${name}.${originalExtension}`;
-		let extension = originalExtension;
+        let name = fileNameParts[0];
+        const originalExtension = fileNameParts[1];
+        const originalFileName = `${name}.${originalExtension}`;
+        let extension = originalExtension;
 
-		let type = file.type;
-		const buffer = await toUint8Array(file);
-		const blob = new Blob([buffer], { type });
+        let type = file.type;
+        const buffer = await toUint8Array(file);
+        const blob = new Blob([buffer], { type });
 
-		await prepareForUpload(name, extension, type, originalFileName, originalExtension, file, blob);
-	}
+        await prepareForUpload(
+            name,
+            extension,
+            type,
+            originalFileName,
+            originalExtension,
+            file,
+            blob
+        );
+    };
 
-	const prepareForUpload = async (name, extension, type, originalFileName, originalFileExtension, file, blob) => {
-		const fileName = `${name}.${extension}`;
+    const prepareForUpload = async (
+        name,
+        extension,
+        type,
+        originalFileName,
+        originalFileExtension,
+        file,
+        blob
+    ) => {
+        const fileName = `${name}.${extension}`;
 
-		const fileAsJSON = {
-			fileName,
-			fileType: type,
-			originalFileName,
-			originalFileExtension,
-		};
+        const fileAsJSON = {
+            fileName,
+            fileType: type,
+            originalFileName,
+            originalFileExtension,
+        };
 
-		const encodedFileName = `${Buffer.from(JSON.stringify(FileAsJSON).toString('base64')}.${extension}`;
+        const encodedFileName = `${Buffer.from(
+            JSON.stringify(fileAsJSON)
+        ).toString('base64')}.${extension}`;
 
-		setFile({
-			name: fileName,
-			filesize: file.size,
-			status: 'uploading',
-			iconDescription: 'uploading',
-		});
+        setFile({
+            name: fileName,
+            filesize: file.size,
+            status: 'uploading',
+            iconDescription: 'uploading',
+        });
 
-		const document = new File(
-			[blob],
-			encodedFileName,
-			{
-				type,
-			},
-		);
+        const document = new File([blob], encodedFileName, {
+            type,
+        });
 
-		await upload(document, originalFileName, blob);
-	}
+        await upload(document, originalFileName, blob);
+    };
 
-	const upload = async (file, fileName, blob) => {
-		const document = new ChunkedFile(file, blob);
+    const upload = async (file, fileName, blob) => {
+        const document = new ChunkedFile(file, blob);
 
-		showNotification({
-			success: true,
-			kind: 'info',
-			subtitle: (
-				<>
-					<p>Your file {fileName} is being uploaded.</p>
-					<p>Please allow a few minutes for the system to process it.</p>
-				</>
-			),
-			timeout: 8000,
-		});
+        showNotification({
+            success: true,
+            kind: 'info',
+            subtitle: (
+                <>
+                    <p>Your file {fileName} is being uploaded.</p>
+                    <p>
+                        Please allow a few minutes for the system to process it.
+                    </p>
+                </>
+            ),
+            timeout: 8000,
+        });
 
-		await document.upload(
-			() => {
-				showNotification({
-					success: true,
-					kind: 'success',
-					subtitle: `File ${fileName} was successfully uploaded.`,
-					timeout: 5000,
-				});
+        await document.upload(
+            () => {
+                showNotification({
+                    success: true,
+                    kind: 'success',
+                    subtitle: `File ${fileName} was successfully uploaded.`,
+                    timeout: 5000,
+                });
 
-				setFile(undefined);
-			},
-			() => {
-				showNotification({
-					success: false,
-					kind: 'error',
-					subtitle: `There was an internal error during upload. Please select a new file and try again.`,
-					timeout: 5000,
-				});
+                setFile(undefined);
+            },
+            () => {
+                showNotification({
+                    success: false,
+                    kind: 'error',
+                    subtitle: `There was an internal error during upload. Please select a new file and try again.`,
+                    timeout: 5000,
+                });
 
-				setFile(undefined);
-			}
-		);
-	}
+                setFile(undefined);
+            }
+        );
+    };
 
-	return (
-		<Container>
-			<strong>Add a file by drag and drop or browse</strong>
-			<p>If you have multiple files, it is recommended to zip them together.</p>
-			<FileUploaderDropContainer
-				id="file-uploader"
-				labelText="Choose a file or drag and drop it here"
-				onAddFiles={handleOnDragNDrop}
-				accept={validTypes}
-				disabled={file !== undefined}
-			/>
-			{file && (
-				<FileUploaderItems>
-						<FileUploaderItem
-							name={file.name}
-							status={file.status}
-							iconDescription={file.iconDescription}
-						/>
-				</FileUploaderItems>
-			)}
-			<p>Max file size is 6MB.</p>
-			<p>Supported file types are .xlsx/.xlsm/.xlsb, .zip, .doc/.docx, .ppt/.pptx, .txt, .pdf, .csv, .print.</p>
-		</Container>
-	)
-}
+    return (
+        <Container>
+            <strong>Add a file by drag and drop or browse</strong>
+            <p>
+                If you have multiple files, it is recommended to zip them
+                together.
+            </p>
+            <FileUploaderDropContainer
+                id="file-uploader"
+                labelText="Choose a file or drag and drop it here"
+                onAddFiles={handleOnDragNDrop}
+                accept={validTypes}
+                disabled={file !== undefined}
+            />
+            {file && (
+                <FileUploaderItems>
+                    <FileUploaderItem
+                        name={file.name}
+                        status={file.status}
+                        iconDescription={file.iconDescription}
+                    />
+                </FileUploaderItems>
+            )}
+            <p>Max file size is 6MB.</p>
+            <p>
+                Supported file types are .xlsx/.xlsm/.xlsb, .zip, .doc/.docx,
+                .ppt/.pptx, .txt, .pdf, .csv, .print.
+            </p>
+        </Container>
+    );
+};
 
 export default FileUploader;
